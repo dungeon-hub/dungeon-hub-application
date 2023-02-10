2215ea180263979f473b75228cbfefb86b50f3f1
Taubsie <unconfigured@null.spigotmc.org>
2023-02-11 00:50:17 +0100

    Moved some code to get easier access to it.

--- src/main/java/me/taubsie/carrylogs/application/listener/MemberJoinListener.java
+++ src/main/java/me/taubsie/carrylogs/application/listener/MemberJoinListener.java
@@ -1,14 +1,8 @@
 package me.taubsie.carrylogs.application.listener;
 
-import me.taubsie.carrylogs.application.enums.IdList;
 import me.taubsie.carrylogs.application.service.ProfileModerationService;
-import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.event.server.member.ServerMemberJoinEvent;
 import org.javacord.api.listener.server.member.ServerMemberJoinListener;
-
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 
 public class MemberJoinListener implements ServerMemberJoinListener
 {
@@ -25,19 +19,7 @@ public class MemberJoinListener implements ServerMemberJoinListener
 
         if (result != null)
         {
-            try
-            {
-                serverMemberJoinEvent.getUser().openPrivateChannel().join().sendMessage("You got banned from Dungeon Hub because of a suspicious user profile.\nIf you think this might be a mistake, please use our unban-form: https://forms.gle/rfQAJueSoyQno1gD6");
-            }
-            catch (Exception exception)
-            {
-                exception.printStackTrace();
-            }
-
-            serverMemberJoinEvent.getServer().banUser(serverMemberJoinEvent.getUser(), Duration.of(6, ChronoUnit.DAYS), "Bad username: " + result);
-
-            Optional<ServerTextChannel> logsChannel = serverMemberJoinEvent.getServer().getTextChannelById(IdList.MODERATION_LOGS_CHANNEL.getId(serverMemberJoinEvent.getServer().getId()));
-            logsChannel.ifPresent(serverTextChannel -> serverTextChannel.sendMessage("User " + serverMemberJoinEvent.getUser().getMentionTag() + " got banned because of a bad username:\n" + result));
+            ProfileModerationService.getInstance().handleUserBan(serverMemberJoinEvent.getServer(), serverMemberJoinEvent.getUser(), result);
         }
     }
 }
