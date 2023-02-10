ebf221d687783702f2423d487077cc987a438b90
Taubsie <unconfigured@null.spigotmc.org>
2023-02-11 00:49:01 +0100

    Added enum for carry roles and a class that should represent a carrier, for easier access to methods.

---
+++ src/main/java/me/taubsie/carrylogs/application/Carrier.java
@@ -0,0 +1,51 @@
+package me.taubsie.carrylogs.application;
+
+import me.taubsie.carrylogs.application.service.ConnectionService;
+import org.javacord.api.entity.user.User;
+
+import java.util.Map;
+
+public class Carrier
+{
+    private final long id;
+
+    public Carrier(long id)
+    {
+        this.id = id;
+    }
+
+    public static Carrier fromUser(User user)
+    {
+        return new Carrier(user.getId());
+    }
+
+    public Map<String, Long> getScore()
+    {
+        return ConnectionService.getInstance().countScore(id);
+    }
+
+    public Long getDungeonScore()
+    {
+        return ConnectionService.getInstance().getDungeonScore(id);
+    }
+
+    public Long getSlayerScore()
+    {
+        return ConnectionService.getInstance().getSlayerScore(id);
+    }
+
+    public Long setScore(String type, long amount)
+    {
+        return ConnectionService.getInstance().modifyScore(id, type, amount);
+    }
+
+    public Long setDungeonScore(long amount)
+    {
+        return ConnectionService.getInstance().modifyDungeonScore(id, amount);
+    }
+
+    public Long setSlayerScore(long amount)
+    {
+        return ConnectionService.getInstance().modifySlayerScore(id, amount);
+    }
+}
