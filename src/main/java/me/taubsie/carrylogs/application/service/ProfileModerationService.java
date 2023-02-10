237186d9bc9674dac1b72ae1dc769623f68d503e
Taubsie <unconfigured@null.spigotmc.org>
2023-02-11 00:50:17 +0100

    Moved some code to get easier access to it.

--- src/main/java/me/taubsie/carrylogs/application/service/ProfileModerationService.java
+++ src/main/java/me/taubsie/carrylogs/application/service/ProfileModerationService.java
@@ -1,11 +1,18 @@
 package me.taubsie.carrylogs.application.service;
 
+import me.taubsie.carrylogs.application.enums.IdList;
 import net.codebox.homoglyph.Homoglyph;
 import net.codebox.homoglyph.HomoglyphBuilder;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ProfileModerationService
@@ -57,6 +64,23 @@ public class ProfileModerationService
         }
 
         return searchResults.stream().map(searchResult -> searchResult.match).collect(Collectors.joining("; "));
+    }
+
+    public void handleUserBan(Server server, User user, String reason)
+    {
+        try
+        {
+            user.openPrivateChannel().join().sendMessage("You got banned from Dungeon Hub because of a suspicious user profile.\nIf you think this might be a mistake, please use our unban-form: https://forms.gle/rfQAJueSoyQno1gD6");
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+
+        server.banUser(user, Duration.of(6, ChronoUnit.DAYS), "Bad username: " + reason);
+
+        Optional<ServerTextChannel> logsChannel = server.getTextChannelById(IdList.MODERATION_LOGS_CHANNEL.getId(server.getId()));
+        logsChannel.ifPresent(serverTextChannel -> serverTextChannel.sendMessage("User " + user.getMentionTag() + " got banned because of a bad username:\n" + reason));
     }
 
     public boolean isOverwritten(long userId)
