a714f36335191eb56742a77ee45e90fb3ec7f5f2
Taubsie <unconfigured@null.spigotmc.org>
2023-02-15 02:03:05 +0100

    Added checks for username changes to prevent more unwanted bots joining the server.

--- src/main/java/me/taubsie/carrylogs/application/service/ProfileModerationService.java
+++ src/main/java/me/taubsie/carrylogs/application/service/ProfileModerationService.java
@@ -11,11 +11,13 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class ProfileModerationService {
+public class ProfileModerationService
+{
     private static final String[] forbiddenUsernames = new String[]{
             "Captcha.bot",
             "Dyno",
@@ -27,20 +29,29 @@ public class ProfileModerationService {
             "Dungeon Hub Bot",
             "Wick"
     };
+    private static final Long[] excludedIds = new Long[]{
+            727320030462869515L,
+            703035551330205716L
+    };
     private static ProfileModerationService instance;
     private final Homoglyph homoglyph;
 
-    ProfileModerationService() {
-        try {
+    ProfileModerationService()
+    {
+        try
+        {
             this.homoglyph = HomoglyphBuilder.build();
         }
-        catch(IOException e) {
+        catch (IOException e)
+        {
             throw new RuntimeException(e);
         }
     }
 
-    public static ProfileModerationService getInstance() {
-        if(instance == null) {
+    public static ProfileModerationService getInstance()
+    {
+        if (instance == null)
+        {
             instance = new ProfileModerationService();
         }
 
@@ -48,23 +59,28 @@ public class ProfileModerationService {
     }
 
     @Nullable
-    public String checkUserName(String userName) {
+    public String checkUserName(String userName)
+    {
         List<Homoglyph.SearchResult> searchResults = homoglyph.search(userName, forbiddenUsernames);
 
-        if(searchResults.isEmpty()) {
+        if (searchResults.isEmpty())
+        {
             return null;
         }
 
         return searchResults.stream().map(searchResult -> searchResult.match).collect(Collectors.joining("; "));
     }
 
-    public void handleUserBan(Server server, User user, String reason) {
-        try {
+    public void handleUserBan(Server server, User user, String reason)
+    {
+        try
+        {
             user.openPrivateChannel().join().sendMessage("You got banned from `" + server.getName() + "` because of a" +
                     " suspicious user profile.\nIf you think this might be a mistake, please appeal at: " +
                     "https://forms.gle/rfQAJueSoyQno1gD6");
         }
-        catch(Exception exception) {
+        catch (Exception exception)
+        {
             exception.printStackTrace();
         }
 
@@ -76,11 +92,13 @@ public class ProfileModerationService {
                 "got banned because of a bad username:\n" + reason));
     }
 
-    public boolean isOverwritten(long userId) {
-        return false;
+    public boolean isOverwritten(long userId)
+    {
+        return Arrays.stream(excludedIds).anyMatch(id -> id == userId);
     }
 
-    public boolean isVerified(User user, Server server) {
+    public boolean isVerified(User user, Server server)
+    {
         return user.getRoles(server)
                 .stream()
                 .anyMatch(role -> role.getId() == IdList.VERIFIED_ROLE.getLocalId(server.getId())
@@ -88,7 +106,13 @@ public class ProfileModerationService {
                 );
     }
 
-    public boolean isExcluded(User user, Server server) {
+    public boolean isExcluded(User user, Server server)
+    {
         return user.isBot() || user.isBotOwnerOrTeamMember() || isOverwritten(user.getId()) || isVerified(user, server);
+    }
+
+    public boolean isExcluded(User user)
+    {
+        return user.isBot() || user.isBotOwnerOrTeamMember() || isOverwritten(user.getId());
     }
 }
