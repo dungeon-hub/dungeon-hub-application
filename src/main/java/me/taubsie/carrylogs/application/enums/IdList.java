f2d3f32a60104235f6bef5491ab7835a3adeabc8
traible <traible@apollon.de>
2023-02-22 14:29:09 +0100

    Added kuudra carry type to prepare for addition of kuudra carries. Also reformatted larger chunks of code.

--- src/main/java/me/taubsie/carrylogs/application/enums/IdList.java
+++ src/main/java/me/taubsie/carrylogs/application/enums/IdList.java
@@ -3,15 +3,16 @@ package me.taubsie.carrylogs.application.enums;
 import lombok.Getter;
 import me.taubsie.carrylogs.CarryRole;
 
-public enum IdList
-{
+public enum IdList {
     SERVER(693263712626278553L, 1023684107877761196L),
     TRANSCRIPTS_CHANNEL(796770232340578365L, 1036375112619937792L),
     APPROVING_CHANNEL(1062481082500522095L, 1036387847000825877L),
     DUNGEON_LOGS_CHANNEL(1021543535834583090L, 1043328648641523825L),
     SLAYER_LOGS_CHANNEL(1048235346422407168L, 1043328648641523825L),
+    KUUDRA_LOGS_CHANNEL(0L /*TODO*/, 0L /*TODO*/),
     DUNGEON_LEADERBOARD_CHANNEL(1063987051986440262L, 1063900837291774032L),
     SLAYER_LEADERBOARD_CHANNEL(1063987003806453781L, 1063900854974955590L),
+    KUUDRA_LEADERBOARD_CHANNEL(0L /*TODO*/, 0L /*TODO*/),
     SCORE_LOGS_CHANNEL(1068265390263767152L, 1067118963831615609L),
     MODERATION_LOGS_CHANNEL(996151183519514814L, 1067118963831615609L),
     F4_ROLE(CarryRole.F4, 793521662678794250L, 1061116185132933240L),
@@ -32,7 +33,8 @@ public enum IdList
     F7_CATEGORY(CarryType.F7, 805833601748828200L, 1026291937440960615L),
     MASTER_CATEGORY(CarryType.MASTER_MODE, 842840550704939053L, 1026291957594587167L),
     EMAN_CATEGORY(CarryType.EMAN, 992922867857641594L, 1026291971872018432L),
-    BLAZE_CATEGORY(CarryType.BLAZE, 992922801075912764L, 1026291986090704968L);
+    BLAZE_CATEGORY(CarryType.BLAZE, 992922801075912764L, 1026291986090704968L),
+    KUUDRA_CATEGORY(CarryType.KUUDRA, 0L /*TODO*/, 0L /*TODO*/);
 
     @Getter
     private final long id;
@@ -42,42 +44,26 @@ public enum IdList
     private CarryType carryType;
     private CarryRole carryRole;
 
-    IdList(long id, long testId)
-    {
+    IdList(long id, long testId) {
         this.id = id;
         this.testId = testId;
     }
 
-    IdList(CarryType carryType, long id, long testId)
-    {
+    IdList(CarryType carryType, long id, long testId) {
         this(id, testId);
 
         this.carryType = carryType;
     }
 
-    IdList(CarryRole carryRole, long id, long testId)
-    {
+    IdList(CarryRole carryRole, long id, long testId) {
         this(id, testId);
 
         this.carryRole = carryRole;
     }
 
-    public CarryType getCarryType()
-    {
-        return carryType;
-    }
-
-    public CarryRole getCarryRole()
-    {
-        return carryRole;
-    }
-
-    public static boolean isCarryCategory(long id, long serverId)
-    {
-        for (IdList idList : values())
-        {
-            if (idList.carryType != null && idList.getLocalId(serverId) == id)
-            {
+    public static boolean isCarryCategory(long id, long serverId) {
+        for(IdList idList : values()) {
+            if(idList.carryType != null && idList.getLocalId(serverId) == id) {
                 return true;
             }
         }
@@ -85,12 +71,9 @@ public enum IdList
         return false;
     }
 
-    public static IdList getCarryCategory(long id, long serverId)
-    {
-        for (IdList idList : values())
-        {
-            if (idList.carryType != null && idList.getLocalId(serverId) == id)
-            {
+    public static IdList getCarryCategory(long id, long serverId) {
+        for(IdList idList : values()) {
+            if(idList.carryType != null && idList.getLocalId(serverId) == id) {
                 return idList;
             }
         }
@@ -98,20 +81,23 @@ public enum IdList
         return null;
     }
 
-    public long getLocalId(long serverId)
-    {
-        if (SERVER.id == serverId)
-        {
+    public CarryType getCarryType() {
+        return carryType;
+    }
+
+    public CarryRole getCarryRole() {
+        return carryRole;
+    }
+
+    public long getLocalId(long serverId) {
+        if(SERVER.id == serverId) {
             return id;
-        }
-        else
-        {
+        } else {
             return testId;
         }
     }
 
-    public IdList[] getSlayerCarryRoles()
-    {
+    public IdList[] getSlayerCarryRoles() {
         return new IdList[]{
                 EMAN_T3_ROLE,
                 EMAN_T4_ROLE,
@@ -121,8 +107,7 @@ public enum IdList
         };
     }
 
-    public IdList[] getDungeonCarryRoles()
-    {
+    public IdList[] getDungeonCarryRoles() {
         return new IdList[]{
                 F4_ROLE,
                 F5_ROLE,
@@ -132,8 +117,12 @@ public enum IdList
         };
     }
 
-    public IdList[] getCarryRoles()
-    {
+    public IdList[] getKuudraCarryRoles() {
+        //TODO add missing roles
+        return new IdList[]{};
+    }
+
+    public IdList[] getCarryRoles() {
         IdList[] result = new IdList[getDungeonCarryRoles().length + getSlayerCarryRoles().length];
 
         System.arraycopy(getDungeonCarryRoles(), 0, result, 0, getDungeonCarryRoles().length);
