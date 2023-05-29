56f6f638500476ff1b28eb763b32895d9ce4358f
Taubsie <tobias@joerg-hartmann.de>
2023-05-29 17:46:14 +0200

    Added more text to carry type and carry tier to be shown in the price messages.

--- src/main/java/me/taubsie/carrylogs/application/enums/CarryTier.java
+++ src/main/java/me/taubsie/carrylogs/application/enums/CarryTier.java
@@ -5,15 +5,15 @@ import java.util.Optional;
 
 public enum CarryTier {
     COMPLETION("Completion"),
-    S("S"),
-    S_PLUS("S+"),
-    FLOOR_1("Floor 1"),
-    FLOOR_2("Floor 2"),
-    FLOOR_3("Floor 3"),
-    FLOOR_4("Floor 4"),
-    FLOOR_5("Floor 5"),
-    FLOOR_6("Floor 6"),
-    FLOOR_7("Floor 7"),
+    S("S", "S Score"),
+    S_PLUS("S+", "S+ Score"),
+    FLOOR_1("Floor 1", "Master 1: Bonzo"),
+    FLOOR_2("Floor 2", "Master 2: Scarf"),
+    FLOOR_3("Floor 3", "Master 3: The Professor"),
+    FLOOR_4("Floor 4", "Master 4: Thorn"),
+    FLOOR_5("Floor 5", "Master 5: Livid"),
+    FLOOR_6("Floor 6", "Master 6: Sadan"),
+    FLOOR_7("Floor 7", "Master 7: The Wither Lords"),
     TIER_2("Tier 2"),
     TIER_3("Tier 3"),
     TIER_4("Tier 4"),
@@ -24,13 +24,23 @@ public enum CarryTier {
     INFERNAL("Infernal");
 
     private final String stringRepresentation;
+    private final String descriptiveName;
+
+    CarryTier(String stringRepresentation, String descriptiveName) {
+        this.stringRepresentation = stringRepresentation;
+        this.descriptiveName = descriptiveName;
+    }
 
     CarryTier(String stringRepresentation) {
-        this.stringRepresentation = stringRepresentation;
+        this(stringRepresentation, stringRepresentation);
     }
 
     public String getStringRepresentation() {
         return stringRepresentation;
+    }
+
+    public String getDescriptiveName() {
+        return descriptiveName;
     }
 
     public static Optional<CarryTier> fromString(String carryTier) {
--- src/main/java/me/taubsie/carrylogs/application/enums/CarryType.java
+++ src/main/java/me/taubsie/carrylogs/application/enums/CarryType.java
@@ -2,36 +2,37 @@ package me.taubsie.carrylogs.application.enums;
 
 import lombok.Getter;
 import org.javacord.api.interaction.SlashCommandOptionChoice;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
 
 public enum CarryType
 {
-    F4("F4", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
+    F4("F4", "Floor 4: Thorn", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
             SlashCommandOptionChoice.create("S", "S"))),
-    F5("F5", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
+    F5("F5", "Floor 5: Livid", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
             SlashCommandOptionChoice.create("S", "S"),
             SlashCommandOptionChoice.create("S+", "S+"))),
-    F6("F6", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
+    F6("F6", "Floor 6: Sadan", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
             SlashCommandOptionChoice.create("S", "S"),
             SlashCommandOptionChoice.create("S+", "S+"))),
-    F7("F7", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
+    F7("F7", "Floor 7: The Wither Lords", Arrays.asList(SlashCommandOptionChoice.create("Completion", "Completion"),
             SlashCommandOptionChoice.create("S", "S"),
             SlashCommandOptionChoice.create("S+", "S+"))),
-    MASTER_MODE("MM", Arrays.asList(SlashCommandOptionChoice.create("Floor 1", "Floor 1"),
+    MASTER_MODE("MM", "Master Mode", Arrays.asList(SlashCommandOptionChoice.create("Floor 1", "Floor 1"),
             SlashCommandOptionChoice.create("Floor 2", "Floor 2"),
             SlashCommandOptionChoice.create("Floor 3", "Floor 3"),
             SlashCommandOptionChoice.create("Floor 4", "Floor 4"),
             SlashCommandOptionChoice.create("Floor 5", "Floor 5"),
             SlashCommandOptionChoice.create("Floor 6", "Floor 6"),
             SlashCommandOptionChoice.create("Floor 7", "Floor 7"))),
-    EMAN("Enderman", Arrays.asList(SlashCommandOptionChoice.create("Tier 3", "Tier 3"),
+    EMAN("Enderman", "Voidgloom Seraph", Arrays.asList(SlashCommandOptionChoice.create("Tier 3", "Tier 3"),
             SlashCommandOptionChoice.create("Tier 4", "Tier 4"))),
-    BLAZE("Blaze", Arrays.asList(SlashCommandOptionChoice.create("Tier 2", "Tier 2"),
+    BLAZE("Blaze", "Inferno Demonlord", Arrays.asList(SlashCommandOptionChoice.create("Tier 2", "Tier 2"),
             SlashCommandOptionChoice.create("Tier 3", "Tier 3"),
             SlashCommandOptionChoice.create("Tier 4", "Tier 4"))),
-    KUUDRA("Kuudra", Arrays.asList(SlashCommandOptionChoice.create("Basic", "Basic"),
+    KUUDRA("Kuudra", "Kuudra", Arrays.asList(SlashCommandOptionChoice.create("Basic", "Basic"),
             SlashCommandOptionChoice.create("Hot", "Hot"),
             SlashCommandOptionChoice.create("Burning", "Burning"),
             SlashCommandOptionChoice.create("Fiery", "Fiery"),
@@ -40,10 +41,13 @@ public enum CarryType
     @Getter
     private final String prettyName;
     @Getter
+    private final String descriptiveName;
+    @Getter
     private final List<SlashCommandOptionChoice> choiceList;
 
-    CarryType(String prettyName, List<SlashCommandOptionChoice> choiceList) {
+    CarryType(String prettyName, String descriptiveName, List<SlashCommandOptionChoice> choiceList) {
         this.prettyName = prettyName;
+        this.descriptiveName = descriptiveName;
         this.choiceList = choiceList;
     }
 
@@ -53,5 +57,13 @@ public enum CarryType
         } catch(IllegalArgumentException illegalArgumentException) {
             return null;
         }
+    }
+
+    @Nullable public String getExtraInformation() {
+        if(this == CarryType.MASTER_MODE) {
+            return "All master mode carries will be S.";
+        }
+
+        return null;
     }
 }
