1ca6b7258d4a11977ca487762b019c314476798c
Taubsie <tobias@joerg-hartmann.de>
2023-11-26 14:29:40 +0100

    Reworked the /help command to merge explanations into there.

---
+++ src/main/java/me/taubsie/dungeonhub/application/classes/HelpDisplay.java
@@ -0,0 +1,18 @@
+package me.taubsie.dungeonhub.application.classes;
+
+import me.taubsie.dungeonhub.application.enums.EmbedColor;
+import me.taubsie.dungeonhub.application.exceptions.CommandExecutionException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record HelpDisplay(@NotNull String description, EmbedColor embedColor, @NotNull Map<String, String> fields) {
+    public static HelpDisplay fromDescription(String description) {
+        return new HelpDisplay(description, EmbedColor.DEFAULT, new HashMap<>());
+    }
+
+    public static HelpDisplay fromException(CommandExecutionException commandExecutionException) {
+        return new HelpDisplay(commandExecutionException.getMessage(), EmbedColor.NEGATIVE, new HashMap<>());
+    }
+}
--- src/main/java/me/taubsie/dungeonhub/application/exceptions/HypixelLinkedToOtherException.java
+++ src/main/java/me/taubsie/dungeonhub/application/exceptions/HypixelLinkedToOtherException.java
@@ -1,8 +1,15 @@
 package me.taubsie.dungeonhub.application.exceptions;
 
 public class HypixelLinkedToOtherException extends CommandExecutionException {
+    private final String ign;
+
+    public HypixelLinkedToOtherException(String ign) {
+        this.ign = ign;
+    }
+
     @Override
     public String getMessage() {
-        return "This account is already linked to another discord user.";
+        return "`" + ign + "` is already linked to another discord-account.\n" +
+                "If you need more information about linking your discord account, please use `/help verification`";
     }
 }
