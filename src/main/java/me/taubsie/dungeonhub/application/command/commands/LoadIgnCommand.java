2c151ace3caa3dfed09124000881e8e72f8de7e6
Taubsie <tobias@joerg-hartmann.de>
2024-04-13 21:31:43 +0200

    Slightly reworked the command that should load the IGN of all users on the server.

--- src/main/java/me/taubsie/dungeonhub/application/command/commands/LoadIgnCommand.java
+++ src/main/java/me/taubsie/dungeonhub/application/command/commands/LoadIgnCommand.java
@@ -6,6 +6,7 @@ import me.taubsie.dungeonhub.application.command.CommandParameters;
 import me.taubsie.dungeonhub.application.connection.DiscordConnection;
 import me.taubsie.dungeonhub.application.connection.HypixelConnection;
 import me.taubsie.dungeonhub.application.connection.MojangConnection;
+import me.taubsie.dungeonhub.application.connection.dungeon_hub.ContentConnection;
 import me.taubsie.dungeonhub.application.connection.dungeon_hub.DiscordUserConnection;
 import me.taubsie.dungeonhub.application.exceptions.NoNameSchemaException;
 import me.taubsie.dungeonhub.application.exceptions.PlayerNotFoundException;
@@ -14,6 +15,7 @@ import me.taubsie.dungeonhub.application.service.NicknameService;
 import me.taubsie.dungeonhub.application.service.RolesService;
 import me.taubsie.dungeonhub.common.model.discord_user.DiscordUserModel;
 import me.taubsie.dungeonhub.common.model.discord_user.DiscordUserUpdateModel;
+import org.javacord.api.entity.DiscordEntity;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
@@ -22,6 +24,7 @@ import org.javacord.api.interaction.SlashCommandOption;
 import org.javacord.api.interaction.SlashCommandOptionBuilder;
 import org.javacord.api.interaction.SlashCommandOptionType;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.Time;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -30,15 +33,44 @@ import java.util.stream.Collectors;
 @CommandParameters(name = "load-ign", description = "Loads the IGN of all users on the server.", enabledForUsers =
         {356134481452597250L})
 public class LoadIgnCommand extends Command {
+    private static final Map<Long, String> fetchingUsers = Collections.synchronizedMap(new HashMap<>());
     private static final Map<Long, UUID> users = Collections.synchronizedMap(new HashMap<>());
+    private static Long serverId;
 
     static {
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
+                fetchingWave();
+
                 loadingWave();
             }
         }, new Time(System.currentTimeMillis() + 500L), 3000L);
+    }
+
+    public static synchronized void fetchingWave() {
+        if (serverId == null) {
+            return;
+        }
+
+        Optional<Server> server = DiscordConnection.getInstance().getBot().getServerById(serverId);
+
+        if (server.isEmpty()) {
+            return;
+        }
+
+        List<Map.Entry<Long, String>> currentWave = fetchingUsers.entrySet().stream().limit(10).toList();
+
+        currentWave.forEach(entry -> {
+            try {
+                users.put(entry.getKey(), MojangConnection.getInstance().getUUIDByName(entry.getValue()));
+            }
+            catch (PlayerNotFoundException playerNotFoundException) {
+                //ignored since we can just ignore players we didn't find
+            }
+        });
+
+        currentWave.stream().map(Map.Entry::getKey).forEach(fetchingUsers::remove);
     }
 
     public static synchronized void loadingWave() {
@@ -85,9 +117,9 @@ public class LoadIgnCommand extends Command {
 
     @Override
     public List<SlashCommandOption> getSlashCommandOptions() {
-        SlashCommandOption showOption = new SlashCommandOptionBuilder()
+        SlashCommandOption progressOption = new SlashCommandOptionBuilder()
                 .setType(SlashCommandOptionType.SUB_COMMAND)
-                .setName("show")
+                .setName("progress")
                 .setDescription("Shows the progress.")
                 .build();
 
@@ -97,7 +129,7 @@ public class LoadIgnCommand extends Command {
                 .setDescription("Starts the loading.")
                 .build();
 
-        return List.of(showOption, startOption);
+        return List.of(progressOption, startOption);
     }
 
     @Override
@@ -105,13 +137,13 @@ public class LoadIgnCommand extends Command {
         boolean show = slashCommandCreateEvent.getSlashCommandInteraction()
                 .getOptionByIndex(0)
                 .map(SlashCommandInteractionOption::getName)
-                .map(s -> s.equalsIgnoreCase("show"))
+                .map(s -> s.equalsIgnoreCase("progress"))
                 .orElse(false);
 
         if (show) {
             respond(ApplicationService.getInstance()
                     .getEmbed()
-                    .setDescription("Progress: " + users.size() + " users left."));
+                    .setDescription("Progress: " + fetchingUsers.size() + " users to fetch and " + users.size() + " users to load left."));
             return;
         }
 
@@ -119,42 +151,54 @@ public class LoadIgnCommand extends Command {
 
         respondLaterEphemeral(completableFuture);
 
+        Server server = getServer();
+
+        serverId = server.getId();
+
         Map<Long, DiscordUserModel> discordUsers = DiscordUserConnection.getInstance()
                 .getAll()
                 .orElse(new ArrayList<>())
                 .stream().collect(Collectors.toMap(DiscordUserModel::getId, discordUserModel -> discordUserModel));
 
-        Server server = getServer();
-
-        server.getMembers().parallelStream()
+        Map<Long, String> usersToFetch = server.getMembers().parallelStream()
                 .filter(user -> !user.isBot())
                 .filter(user -> !discordUsers.containsKey(user.getId()) || discordUsers.get(user.getId()).getMinecraftId() == null)
-                .forEach(user -> user.getNickname(server)
-                        .ifPresent(s -> {
-                            String ign = s.replaceAll("❮(\\S*)❯", "")
-                                    .replaceAll("\\[(\\S*)]", "")
-                                    .replace("★", "")
-                                    .replace("✦", "")
-                                    .replace("✶", "")
-                                    .replace("✽", "")
-                                    .replace("❊", "")
-                                    .strip();
+                .collect(Collectors.toMap(
+                        DiscordEntity::getId,
+                        o -> o.getDisplayName(server)
+                                .replaceAll("❮(\\S*)❯", "")
+                                .replaceAll("\\[(\\S*)]", "")
+                                .replace("★", "")
+                                .replace("✦", "")
+                                .replace("✶", "")
+                                .replace("✽", "")
+                                .replace("❊", "")
+                                .replace("✯", "")
+                                .replace("✩", "")
+                                .strip()
+                ));
 
-                            try {
-                                users.put(user.getId(), MojangConnection.getInstance().getUUIDByName(ign));
-                            }
-                            catch (PlayerNotFoundException playerNotFoundException) {
-                                //ignored since we can just ignore players we didn't find
-                            }
-                        }));
+        System.out.println("★");
+
+        fetchingUsers.putAll(usersToFetch);
+
+        String description = fetchingUsers.entrySet().stream()
+                .map(entry -> "<@" + entry.getKey() + ">: `" + entry.getValue() + "`")
+                .collect(Collectors.joining("\n"));
+
+        if (description.length() >= 4000) {
+            description = "The list of igns to fetch would be too long.\n"
+                    + ContentConnection.getInstance()
+                    .uploadFile(description.getBytes(StandardCharsets.UTF_8))
+                    .map(s -> "https://cdn.dungeon-hub.net/" + s)
+                    .orElse("The full list has been logged, contact administrators for more information.");
+        }
 
         completableFuture.complete(
                 DelayedResponse.fromEmbed(
                         ApplicationService.getInstance()
                                 .getEmbed()
-                                .setDescription(users.entrySet().stream()
-                                        .map(entry -> "<@" + entry.getKey() + ">: `" + entry.getValue() + "`")
-                                        .collect(Collectors.joining("\n"))))
+                                .setDescription(description))
         );
     }
 }
