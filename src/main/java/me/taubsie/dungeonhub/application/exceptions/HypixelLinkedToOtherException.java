9a446c8d3fd67938344b67f5e84ace6338a74fbd
Taubsie <tobias@joerg-hartmann.de>
2024-04-17 00:32:36 +0200

    Rephrased an exception to avoid confusion.

--- src/main/java/me/taubsie/dungeonhub/application/exceptions/HypixelLinkedToOtherException.java
+++ src/main/java/me/taubsie/dungeonhub/application/exceptions/HypixelLinkedToOtherException.java
@@ -3,7 +3,7 @@ package me.taubsie.dungeonhub.application.exceptions;
 public class HypixelLinkedToOtherException extends CommandExecutionException {
 
     public HypixelLinkedToOtherException(String ign) {
-        super("`" + ign + "` is already linked to another discord-account.\n" +
+        super("`" + ign + "` has their ingame discord-account set to something else.\n" +
                 "If you need more information about linking your discord account, please use `/help verification`");
     }
 }
