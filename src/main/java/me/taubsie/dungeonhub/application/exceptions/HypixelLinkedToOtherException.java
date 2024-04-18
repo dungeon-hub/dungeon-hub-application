5a7bb83c25641dd5d73bdb9e85b3524849079c6f
Taubsie <tobias@joerg-hartmann.de>
2024-04-18 12:02:55 +0200

    Slightly rephrased an error message.

--- src/main/java/me/taubsie/dungeonhub/application/exceptions/HypixelLinkedToOtherException.java
+++ src/main/java/me/taubsie/dungeonhub/application/exceptions/HypixelLinkedToOtherException.java
@@ -8,7 +8,7 @@ public class HypixelLinkedToOtherException extends CommandExecutionException {
 
     public HypixelLinkedToOtherException(String ign, String wrongUsername, String actualUsername) {
         super("`" + ign + "` has their ingame discord-account set to `" + wrongUsername + "`.\n" +
-                "If `" + ign + "` is your account, please change it to `" + actualUsername + "` and wait for it to update.\n" +
+                "If that is your account, please change it to `" + actualUsername + "` and wait for it to update.\n" +
                 "If you need more information about linking your discord account, please use `/help verification`");
     }
 }
