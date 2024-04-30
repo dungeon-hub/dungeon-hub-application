eb9eab33b92bd8a60d01410f74142a920c7385dd
Taubsie <tobias@joerg-hartmann.de>
2024-04-30 15:10:44 +0200

    Fully completed the migration of the auto-completion listener

--- src/main/java/me/taubsie/dungeonhub/kord/application/service/AutoCompletion.kt
+++ src/main/java/me/taubsie/dungeonhub/kord/application/service/AutoCompletion.kt
@@ -3,10 +3,11 @@ package me.taubsie.dungeonhub.kord.application.service
 import com.kotlindiscord.kord.extensions.commands.converters.AutoCompleteCallback
 import dev.kord.common.entity.Choice
 import dev.kord.common.entity.optional.Optional
+import dev.kord.core.behavior.channel.asChannelOfOrNull
 import dev.kord.core.behavior.interaction.suggest
+import dev.kord.core.entity.channel.CategorizableChannel
 import dev.kord.core.event.interaction.AutoCompleteInteractionCreateEvent
-import me.taubsie.dungeonhub.application.connection.dungeon_hub.CarryTypeConnection
-import me.taubsie.dungeonhub.application.connection.dungeon_hub.PurgeTypeConnection
+import me.taubsie.dungeonhub.application.connection.dungeon_hub.*
 
 object AutoCompletion {
     val carryType: AutoCompleteCallback = { event ->
@@ -29,6 +30,92 @@ object AutoCompletion {
                 }
                 .toList()
         )
+    }
+
+    val carryTier: AutoCompleteCallback = { event ->
+        val allOptions = event.interaction.command.options
+
+        val carryType: String? = allOptions.filter { entry ->
+            entry.key.equals("carry-type", ignoreCase = true)
+        }.values.firstOrNull()?.value as String?
+
+        if (carryType != null) {
+            CarryTypeConnection.getInstance(event.getGuildId())
+                .getByIdentifier(carryType)
+                .flatMap { carryTypeModel ->
+                    CarryTierConnection.getInstance(carryTypeModel).allCarryTiers
+                }
+                .orElse(listOf())
+                .stream()
+                .map { carryTier ->
+                    Choice.StringChoice(
+                        name = carryTier.displayName,
+                        value = carryTier.identifier,
+                        nameLocalizations = Optional()
+                    )
+                }
+                .toList()
+        }
+
+        listOf<Choice>()
+    }
+
+    val carryDifficulty: AutoCompleteCallback = { event ->
+        val allOptions = event.interaction.command.options
+
+        val carryType: String? = allOptions.filter { entry ->
+            entry.key.equals("carry-type", ignoreCase = true)
+        }.values.firstOrNull()?.value as String?
+
+        val carryTier: String? = allOptions.filter { entry ->
+            entry.key.equals("carry-tier", ignoreCase = true)
+        }.values.firstOrNull()?.value as String?
+
+        if (carryTier != null) {
+            val carryTierModel = CarryTypeConnection.getInstance(event.getGuildId()).getByIdentifier(carryType)
+                .flatMap { carryTypeModel ->
+                    CarryTierConnection.getInstance(carryTypeModel).getByIdentifier(carryTier)
+                }.orElse(null)
+
+            if (carryTierModel != null) {
+                CarryDifficultyConnection.getInstance(carryTierModel)
+                    .allCarryDifficulties
+                    .orElse(listOf())
+                    .stream()
+                    .map { carryDifficulty ->
+                        Choice.StringChoice(
+                            name = carryDifficulty.displayName,
+                            value = carryDifficulty.identifier,
+                            nameLocalizations = Optional()
+                        )
+                    }
+                    .toList()
+            }
+        } else {
+            val categoryId = event.interaction.channel.asChannelOfOrNull<CategorizableChannel>()?.categoryId
+
+            val carryTierByCategory = categoryId?.let { category ->
+                DiscordServerConnection.getInstance()
+                    .getCarryTierFromCategory(event.getGuildId(), category.value.toLong())
+            }?.orElse(null)
+
+            if (carryTierByCategory != null) {
+                CarryDifficultyConnection.getInstance(carryTierByCategory)
+                    .allCarryDifficulties
+                    .orElse(listOf())
+                    .stream()
+                    .map { carryDifficulty ->
+                        Choice.StringChoice(
+                            name = carryDifficulty.displayName,
+                            value = carryDifficulty.identifier,
+                            nameLocalizations = Optional()
+                        )
+                    }
+                    .toList()
+            }
+        }
+
+        listOf<Choice>()
     }
 
     val purgeType: AutoCompleteCallback = { event ->
