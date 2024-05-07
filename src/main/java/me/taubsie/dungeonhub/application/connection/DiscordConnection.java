22d55bd890f798042ef35cb6d2d78ec8bf76e956
Taubsie <tobias@joerg-hartmann.de>
2024-05-07 15:25:25 +0200

    Cleared the old discord connection and officially moved the main class to the new discord connection.

--- src/main/java/me/taubsie/dungeonhub/application/connection/DiscordConnection.java
+++ src/main/java/me/taubsie/dungeonhub/application/connection/DiscordConnection.java
@@ -1,39 +1,12 @@
 package me.taubsie.dungeonhub.application.connection;
 
 import lombok.Getter;
-import me.taubsie.dungeonhub.application.loader.ClassLoaderService;
 import org.javacord.api.DiscordApi;
-import org.javacord.api.entity.activity.ActivityType;
-import org.javacord.api.entity.server.Server;
-import org.javacord.api.entity.user.UserStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * This is the main-class for the application.
- * It automatically loads all listeners and commands and initiates the bot-instance.
- * Even if it doesn't make much of a difference, it is advised to not use {@link #getBot()} too much.
- *
- * @author Taubsie
- * @since 1.0.0
- */
 @Getter
 public class DiscordConnection {
-    private static final Logger logger = LoggerFactory.getLogger(DiscordConnection.class);
-    private static final String LINE = "----------------------------------------";
-
     private static DiscordConnection instance;
     private DiscordApi bot;
-
-    /**
-     * The main-method that is executed by the JVM.
-     *
-     * @param args The command-line parameters passed by the JVM.
-     */
-    public static void main(String[] args) {
-        ClassLoaderService.getInstance().loadStartupListeners();
-        ClassLoaderService.getInstance().executeStartup();
-    }
 
     /**
      * Returns the current instance of this class.
