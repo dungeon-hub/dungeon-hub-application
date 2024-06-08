e5af19fc9c65312c14a4219678d85d6de0c6b10e
Taubsie <tobias@joerg-hartmann.de>
2024-06-08 17:54:17 +0200

    Removed some unused methods.

--- src/main/java/me/taubsie/dungeonhub/application/service/ApplicationService.java
+++ src/main/java/me/taubsie/dungeonhub/application/service/ApplicationService.java
@@ -1,34 +1,21 @@
 package me.taubsie.dungeonhub.application.service;
 
 import lombok.extern.slf4j.Slf4j;
-import me.taubsie.dungeonhub.application.classes.FlagResponse;
-import me.taubsie.dungeonhub.application.command.Command;
 import me.taubsie.dungeonhub.application.connection.DiscordConnection;
 import me.taubsie.dungeonhub.application.enums.EmbedColor;
-import me.taubsie.dungeonhub.application.loader.ClassLoaderService;
 import me.taubsie.dungeonhub.common.DungeonHubService;
 import me.taubsie.dungeonhub.common.StrikeData;
-import me.taubsie.dungeonhub.common.enums.ScoreType;
-import me.taubsie.dungeonhub.common.model.carry.CarryModel;
 import me.taubsie.dungeonhub.common.model.carry_difficulty.CarryDifficultyModel;
-import me.taubsie.dungeonhub.common.model.carry_queue.CarryQueueModel;
 import me.taubsie.dungeonhub.common.model.carry_tier.CarryTierModel;
 import me.taubsie.dungeonhub.common.model.carry_type.CarryTypeModel;
 import me.taubsie.dungeonhub.common.model.discord_role.DiscordRoleModel;
-import me.taubsie.dungeonhub.common.model.score.ScoreModel;
 import me.taubsie.dungeonhub.kord.application.exceptions.CommandExecutionException;
 import org.javacord.api.entity.Nameable;
 import org.javacord.api.entity.message.MessageFlag;
 import org.javacord.api.entity.message.component.*;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
-import org.javacord.api.event.interaction.SlashCommandCreateEvent;
 import org.javacord.api.interaction.InteractionBase;
-import org.javacord.api.interaction.SlashCommandOption;
-import org.javacord.api.interaction.SlashCommandOptionBuilder;
-import org.javacord.api.interaction.SlashCommandOptionType;
-import org.jetbrains.annotations.NotNull;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -38,7 +25,6 @@ import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class ApplicationService {
-    private static final int MAX_MINECRAFT_USERNAME_LENGTH = 16;
     private static ApplicationService instance;
 
     public static ApplicationService getInstance() {
@@ -55,7 +41,6 @@ public class ApplicationService {
 
     /**
      * Returns the default footer used for most embeds.
-     * Warning is suppressed, since the escape needs to be made due to some systems having an issue showing the unicode representation through discord.
      *
      * @return the default footer used for most embeds.
      */
@@ -65,7 +50,6 @@ public class ApplicationService {
 
     /**
      * Returns the footer used for price message embeds.
-     * Warning is suppressed, since the escape needs to be made due to some systems having an issue showing the unicode representation through discord.
      *
      * @return the footer used for price message embeds.
      */
@@ -127,23 +111,12 @@ public class ApplicationService {
         return getErrorEmbed().setDescription(commandExecutionException.getMessage());
     }
 
-    public EmbedBuilder getErrorEmbed(EmbedBuilder embed, CommandExecutionException commandExecutionException) {
-        return getErrorEmbed(embed).setDescription(commandExecutionException.getMessage());
-    }
-
     public void respondWithError(InteractionBase interactionBase,
                                  CommandExecutionException commandExecutionException) {
         interactionBase.createImmediateResponder()
                 .setFlags(MessageFlag.EPHEMERAL)
                 .addEmbed(getErrorEmbed(commandExecutionException))
                 .respond();
-    }
-
-    public Optional<Command> getCommand(SlashCommandCreateEvent slashCommandCreateEvent) {
-        return ClassLoaderService.getInstance().getCommand(
-                slashCommandCreateEvent.getSlashCommandInteraction().getCommandName(),
-                slashCommandCreateEvent.getSlashCommandInteraction().getServer().orElse(null)
-        );
     }
 
     public EmbedBuilder loadEmbedFromDiscordRole(DiscordRoleModel discordRoleModel) {
@@ -155,52 +128,6 @@ public class ApplicationService {
         embed.addInlineField("Verified role", discordRoleModel.isVerifiedRole() ? "yes" : "no");
 
         return embed;
-    }
-
-    public EmbedBuilder loadEmbedFromCarry(CarryModel carry, EmbedBuilder embedBuilder) {
-        embedBuilder.setColor(EmbedColor.INFORMATION.getColor())
-                .addInlineField("Number of carries",
-                        String.valueOf(carry.amount()))
-                .addInlineField("Type of carry",
-                        carry.carryDifficulty().getCarryTier().getDisplayName() + " - " + carry.carryDifficulty().getDisplayName())
-                .addInlineField("Player", "<@" + carry.player().getId() + ">")
-                .addInlineField("Carrier", "<@" + carry.carrier().getId() + ">");
-
-        if (carry.approver() != null) {
-            embedBuilder.addInlineField("Approved by", "<@" + carry.approver() + ">");
-        }
-
-        if (carry.attachmentLink() != null) {
-            embedBuilder.addInlineField("Transcript-Link", "[Click to open]" +
-                    "(" + carry.attachmentLink() + ")");
-        }
-
-        return embedBuilder;
-    }
-
-    public EmbedBuilder loadEmbedFromCarryQueue(CarryQueueModel carryQueue, EmbedBuilder embedBuilder) {
-        embedBuilder.setColor(EmbedColor.INFORMATION.getColor())
-                .addInlineField("Number of carries",
-                        String.valueOf(carryQueue.getAmount()))
-                .addInlineField("Type of carry",
-                        carryQueue.getCarryTier().getDisplayName() + " - " + carryQueue.getCarryDifficulty().getDisplayName())
-                .addInlineField("Player", "<@" + carryQueue.getPlayer().getId() + ">")
-                .addInlineField("Carrier", "<@" + carryQueue.getCarrier().getId() + ">");
-
-        if (carryQueue.getAttachmentLink() != null) {
-            embedBuilder.addInlineField("Transcript-Link", "[Click to open]" +
-                    "(" + carryQueue.getAttachmentLink() + ")");
-        }
-
-        return embedBuilder;
-    }
-
-    public EmbedBuilder loadEmbedFromCarry(CarryModel carry) {
-        return loadEmbedFromCarry(carry, getEmbed(carry.time()));
-    }
-
-    public EmbedBuilder loadEmbedFromCarryQueue(@NotNull CarryQueueModel carryQueue) {
-        return loadEmbedFromCarryQueue(carryQueue, getEmbed(carryQueue.getTime()));
     }
 
     public EmbedBuilder formatStrikes(List<StrikeData> strikeData, User user, int page) {
@@ -291,76 +218,6 @@ public class ApplicationService {
         return embedBuilder;
     }
 
-    public EmbedBuilder getNoCarryTypeFoundEmbed() {
-        return getEmbed()
-                .setTitle("No score was found!")
-                .setDescription("Please make sure that a carry type is setup on this server.\n" +
-                        "For more information about how to do this, contact `@taubsie` (<@356134481452597250>)!")
-                .setColor(EmbedColor.NEGATIVE.getColor());
-    }
-
-    public EmbedBuilder getScoreCountMessage(User userToCheck, User user, Server server, List<ScoreModel> scoreCount) {
-        if (scoreCount.isEmpty()) {
-            return getNoCarryTypeFoundEmbed();
-        }
-
-        EmbedBuilder embed = getEmbed()
-                .setTitle((userToCheck.getId() != user.getId() && server != null)
-                        ? userToCheck.getDisplayName(server) + "'s score:"
-                        : "Your score:")
-                .setColor(EmbedColor.DEFAULT.getColor());
-
-        Map<ScoreType, List<String>> scoreDescriptions = new EnumMap<>(ScoreType.class);
-
-        scoreCount.forEach(scoreModel -> {
-            if (scoreModel.getScoreType() == ScoreType.EVENT && !scoreModel.getCarryType().isEventActive()) {
-                return;
-            }
-
-            String description = scoreModel.getCarryType().getDisplayName() + ": " + scoreModel.getScoreAmount();
-
-            if (scoreDescriptions.containsKey(scoreModel.getScoreType())) {
-                scoreDescriptions.get(scoreModel.getScoreType()).add(description);
-            } else {
-                scoreDescriptions.put(scoreModel.getScoreType(), new ArrayList<>(List.of(description)));
-            }
-        });
-
-        scoreDescriptions
-                .forEach((carryType, strings) -> embed.addInlineField(
-                        carryType.getDisplayName(),
-                        String.join(System.lineSeparator(), strings)
-                ));
-
-        return embed;
-    }
-
-    public SlashCommandOption getIngamenameOption() {
-        return new SlashCommandOptionBuilder()
-                .setName("ign")
-                .setDescription("The users ingame-name")
-                .setType(SlashCommandOptionType.STRING)
-                .setMinLength(2)
-                .setRequired(true)
-                .build();
-    }
-
-    public String formatFlagDetails(List<FlagResponse> flagged) {
-        List<String> result = new ArrayList<>();
-
-        for (FlagResponse flagResponse : flagged) {
-            if (flagResponse.discord() != null && flagResponse.discord().flagged()) {
-                result.add("- " + flagResponse.name() + " (by discord): " + flagResponse.discord().format());
-            }
-
-            if (flagResponse.uuid() != null && flagResponse.uuid().flagged()) {
-                result.add("- " + flagResponse.name() + " (by UUID): " + flagResponse.uuid().format());
-            }
-        }
-
-        return String.join("\n", result);
-    }
-
     public EmbedBuilder getCarryTypeEmbed(CarryTypeModel carryType) {
         EmbedBuilder embed = getEmbed()
                 .setColor(EmbedColor.DEFAULT.getColor())
@@ -415,17 +272,6 @@ public class ApplicationService {
         carryDifficulty.getActualPriceName().ifPresent(s -> embed.addInlineField("Price Title", s));
 
         return embed;
-    }
-
-    public HighLevelComponent getLinkModalComponent() {
-        return new ActionRowBuilder().addComponents(
-                new TextInputBuilder(TextInputStyle.SHORT, "ign", "Ingame-Name")
-                        .setMaximumLength(MAX_MINECRAFT_USERNAME_LENGTH)
-                        .setMinimumLength(3)
-                        .setPlaceholder("For example: Taubsie")
-                        .setRequired(true)
-                        .build()
-        ).build();
     }
 
     public HighLevelComponent getLinkButtons() {
