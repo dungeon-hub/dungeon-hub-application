a33fbbd743d9331595e3d34416ed7714ca542069
Taubsie <tobias@joerg-hartmann.de>
2024-06-21 16:15:30 +0200

    Switched the transcript generator to the one that is published on maven central.

---
+++ src/main/java/me/taubsie/dungeonhub/kord/application/commands/TestTranscriptCommand.kt
@@ -0,0 +1,70 @@
+package me.taubsie.dungeonhub.kord.application.commands
+
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
+import dev.kord.core.behavior.channel.asChannelOfOrNull
+import dev.kord.core.entity.channel.GuildMessageChannel
+import me.taubsie.dungeonhub.application.connection.dungeon_hub.ContentConnection
+import me.taubsie.dungeonhub.kord.application.config.ConfigProperty
+import me.taubsie.dungeonhub.kord.application.loader.LoadExtension
+import me.taubsie.dungeonhub.kord.application.service.ApplicationService
+import net.dungeonhub.wrapper.kord.createTranscript
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
+@LoadExtension
+class TestTranscriptCommand: Extension() {
+    override val name = "test-transcript-command"
+    private val allowedUsers = listOf(
+        356134481452597250L
+    )
+    private val logger = LoggerFactory.getLogger(TestTranscriptCommand::class.java)
+
+    override suspend fun setup() {
+        ephemeralSlashCommand {
+            name = "test-transcript"
+            description = "Tests the transcript feature."
+            allowInDms = false
+            check {
+                failIfNot("You aren't allowed to use this command.") {
+                    allowedUsers.contains(event.interaction.user.id.value.toLong())
+                }
+            }
+
+            action {
+                respond {
+                    val transcriptChannel = channel.asChannelOfOrNull<GuildMessageChannel>()
+
+                    if(transcriptChannel == null) {
+                        val embed = ApplicationService.errorEmbed
+                        embed.description = "Please use this in a server text channel."
+                        embeds = mutableListOf(embed)
+                        return@respond
+                    }
+
+                    try {
+                        val transcript = transcriptChannel.createTranscript()
+
+                        val url = ContentConnection.getInstance()
+                            .uploadFile(transcript.toByteArray(StandardCharsets.UTF_8))
+                            .orElse(null)
+
+                        if (!url.isNullOrBlank()) {
+                            val embed = ApplicationService.embed
+                            embed.description = "Embed > ${ConfigProperty.CDN_URL}$url"
+                            embeds = mutableListOf(embed)
+                            return@respond
+                        }
+                    } catch (ioException: IOException) {
+                        logger.error(null, ioException)
+                    }
+
+                    val embed = ApplicationService.errorEmbed
+                    embed.description = "Couldn't generate transcript."
+                    embeds = mutableListOf(embed)
+                }
+            }
+        }
+    }
+}
