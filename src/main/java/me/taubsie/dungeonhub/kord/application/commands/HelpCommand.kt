55f57c39560027922e743b9d35df234ff37483c7
Taubsie <tobias@joerg-hartmann.de>
2024-07-17 23:38:59 +0200

    Rephrased and reordered the /help command.

--- src/main/java/me/taubsie/dungeonhub/kord/application/commands/HelpCommand.kt
+++ src/main/java/me/taubsie/dungeonhub/kord/application/commands/HelpCommand.kt
@@ -74,11 +74,11 @@ class HelpCommand : Extension() {
                             This bot uses slash commands, in order to use it you must have your discord client updated (No need to worry if you're on desktop).
                                                     
                             **Usage:** 
-                            `/log amount:NUMBER carry-type:Completion/S/S+/Tier 2/Tier 3/Tier 4` - Run this inside the ticket you are logging to log your carries and earn score.
+                            `/help <topic>` - Displays more information about the selected topic, e.g. the score system.
+                            `/log <amount> <carry-type (e.g. Completion | S | S+ | Tier 3 | Tier 4)>` - Run this inside the ticket you are logging to log your carries and earn score.
                             `/score` - Displays your current score.
-                            `/score-help` - Displays more information about the score system.
-                            `/calc-price type:TYPE tier:TIER amount:AMOUNT` - Calculates the price of carries.
-                            `/leaderboard leaderboard:TYPE` - Shows a leaderboard containing either the current or the all-time score.
+                            `/leaderboard <leaderboard>` - Shows a leaderboard containing either the current or the all-time score.
+                            `/calc-price <type> <tier> <amount>` - Calculates the price of carries.
                             """.trimIndent()
 
             return embed
