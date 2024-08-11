ab5217a392bbdde17419b81ab6e1efb4182b301c
Taubsie <tobias@joerg-hartmann.de>
2024-08-12 01:25:52 +0200

    Added a functionality to have static channels that show some stats about the server (unfortunately only in a pretty hardcoded way :( ).

---
+++ src/main/kotlin/me/taubsie/dungeonhub/application/service/ServerStatsService.kt
@@ -0,0 +1,106 @@
+package me.taubsie.dungeonhub.application.service
+
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.behavior.channel.asChannelOfOrNull
+import dev.kord.core.behavior.channel.edit
+import dev.kord.core.behavior.getChannelOfOrNull
+import dev.kord.core.entity.Guild
+import dev.kord.core.entity.channel.GuildChannel
+import dev.kord.core.entity.channel.TextChannel
+import dev.kord.core.entity.channel.VoiceChannel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import me.taubsie.dungeonhub.application.connection.DiscordConnection
+import me.taubsie.dungeonhub.application.connection.dungeon_hub.DiscordServerConnection
+import me.taubsie.dungeonhub.application.connection.dungeon_hub.DiscordUserConnection
+import me.taubsie.dungeonhub.application.connection.dungeon_hub.getTotalAmountOfMoneySpent
+import me.taubsie.dungeonhub.application.loader.OnStart
+import me.taubsie.dungeonhub.application.loader.StartupListener
+import org.slf4j.LoggerFactory
+import java.sql.Time
+import java.util.*
+import kotlin.concurrent.thread
+
+@OnStart
+object ServerStatsService : StartupListener {
+    private val logger = LoggerFactory.getLogger(ServerStatsService::class.java)
+    private var timer: Timer? = null
+    private val serverStatChannels = listOf(
+        1023684107877761196L to listOf(
+            1272308210102964254L to "{linked_users} Linked Users",
+            1272323673457557616 to "{spent_money} coins spent"
+        ),
+        693263712626278553L to listOf(
+            1272331486154194984L to "{linked_users} Linked Users",
+            1272331662772142081L to "{spent_money} coins spent"
+        )
+    )
+
+    override suspend fun postStart() {
+        resetTimer()
+    }
+
+
+    private fun resetTimer() {
+        if (timer != null) {
+            timer!!.cancel()
+        }
+
+        timer = Timer()
+
+        timer!!.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                logger.debug("Server stat channels reloaded!")
+
+                thread {
+                    runBlocking {
+                        launch {
+                            loadServerStatChannels()
+                        }
+                    }
+                }
+            }
+        }, Time(System.currentTimeMillis() + 1000 * 60), 1000L * 60 * 15)
+    }
+
+    private suspend fun loadServerStatChannels() {
+        serverStatChannels.forEach { server ->
+            DiscordConnection.bot!!.kordRef.getGuildOrNull(Snowflake(server.first))
+                ?.let {
+                    updateStatChannels(it, server.second)
+                }
+        }
+    }
+
+    private suspend fun updateStatChannels(guild: Guild, channels: List<Pair<Long, String>>) {
+        val linkedUsers = DiscordUserConnection.getInstance().countLinkedUsers().orElse("0")
+        val spentMoney = try {
+            ApplicationService.makeNumberReadable(
+                DiscordServerConnection.getInstance().getTotalAmountOfMoneySpent(guild.id.value.toLong())
+            )
+        } catch (ex: Exception) {
+            0
+        }
+
+        channels.forEach { channel ->
+            guild.getChannelOfOrNull<GuildChannel>(Snowflake(channel.first))
+                ?.let { guildChannel ->
+                    val newName = channel.second
+                        .replace("{linked_users}", linkedUsers)
+                        .replace("{spent_money}", spentMoney.toString())
+
+                    guildChannel.asChannelOfOrNull<TextChannel>()?.let { textChannel ->
+                        textChannel.edit {
+                            name = newName
+                        }
+                    }
+
+                    guildChannel.asChannelOfOrNull<VoiceChannel>()?.let { voiceChannel ->
+                        voiceChannel.edit {
+                            name = newName
+                        }
+                    }
+                }
+        }
+    }
+}
