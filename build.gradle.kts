d4fb8e6d821a3d36bfc66162ee8e65784e2c08af
Taubsie <tobias@joerg-hartmann.de>
2024-11-07 11:02:25 +0100

    Bump the API model version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
     //Internal API
     implementation("me.taubsie:dungeon-hub-common:1.0.0")
-    implementation("net.dungeon-hub.api:model:0.1")
+    implementation("net.dungeon-hub.api:model:0.1.1")
 
     //Functionality
     implementation("net.dungeon-hub:transcripts-kord:0.1.1")
